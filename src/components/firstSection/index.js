b543c79230b84800a53e6bf812cf102ff6fdc2ba
wendellima94 <wendel.e.s.lima@gmail.com>
2021-02-10 10:34:45 -0300

    Alterando para responsivo

--- src/components/firstSection/index.js
+++ src/components/firstSection/index.js
@@ -14,9 +14,9 @@ const  FirstSection = () => {
             
                 <TextStyle>Olá, eu sou <br/>Wendel de Lima <br/> Desenvolvedor Web Full Stack</TextStyle><br/>
                 <IconsContainer>
-                <a href="https://github.com/wendellima94" target="_blank">
+                <a href="https://github.com/wendellima94" target="_blank" rel="noreferrer">
                     <img src={GitIcon} alt="git-icon"  /></a>
-                <a href="https://www.linkedin.com/in/wendel-de-lima-74b6b01a1/" target="_blank">
+                <a href="https://www.linkedin.com/in/wendel-de-lima-74b6b01a1/" target="_blank" rel="noreferrer">
                     <img src={LinkedinIcon} alt="linkedin-icon" /></a>
 
                 {/* <a href="" target="_blank">
