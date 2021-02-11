d7688b008316b6ba0d93b359dd45e4899d8dadf1
wendellima94 <wendel.e.s.lima@gmail.com>
2021-02-11 08:08:28 -0300

    Atualizando projetos

--- src/components/fifthSection/index.js
+++ src/components/fifthSection/index.js
@@ -3,7 +3,9 @@ import React from 'react';
 import { Container, TitleText, ImgResolution, ButtonGit, ContainerProjetos } from './styled';
 
 import TelaPC from '../../assets/telaPc.png';
+import TelaPcDois from '../../assets/TelaPcDois.png';
 import Tablet from '../../assets/tablet.png';
+import Mouse from '../../assets/mouse.png';
 
 const FifthSection = () => {
   return (
@@ -26,6 +28,24 @@ const FifthSection = () => {
           <h3>Front-End</h3><br></br>
             <ButtonGit 
             href='https://github.com/wendellima94/Urna-eletronica/tree/urnaEletronica' 
+            target="_blank"
+            >
+              Ver no GitHub
+            </ButtonGit>
+          <ImgResolution src={TelaPcDois} alt="imagem-Tablet" />
+          <h1>Astro Match </h1><br></br>
+          <h3>Front-End</h3><br></br>
+            <ButtonGit 
+            href='https://github.com/wendellima94/AstroMatch' 
+            target="_blank"
+            >
+              Ver no GitHub
+            </ButtonGit>
+          <ImgResolution src={Mouse} alt="imagem-Tablet" />
+          <h1>Alura Quiz</h1><br></br>
+          <h3>Front-End</h3><br></br>
+            <ButtonGit 
+            href='https://github.com/wendellima94/alura-quiz' 
             target="_blank"
             >
               Ver no GitHub
